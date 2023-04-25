05ecbce9603b70269810732a1cd61d96ab078d14
anushkavhatkar <vhata986@newschool.edu>
2023-04-25 19:22:03 -0400

    updated file names

---
+++ index.js
@@ -0,0 +1,12 @@
+let colorArray = ['#00CF08', '#C9E518', '#EBEE6D', '#6AFBA4', '#5DD8B3', '#00CF08', '#315B51', '#4C7345', '#758C6A', '#8BB04E'];
+
+let mainContainer = document.querySelector('#container');
+
+containerDOM = "<div>";
+
+containerDOM += `<div class="container">`; 
+
+for (i=0, i<100, i++) { 
+    containerDOM += `<div class="one"></div>`
+}
+
