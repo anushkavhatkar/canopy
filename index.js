316f9e86d3aa22174b81239fe798509b7149fe45
anushkavhatkar <vhata986@newschool.edu>
2023-04-26 00:01:55 -0400

    landing page

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ let colorArray = ['#00CF08', '#C9E518', '#EBEE6D', '#6AFBA4', '#5DD8B3', '#00CF0
 
 let mainContainer = document.querySelector('#container');
 
-containerDOM = "<div>";
+window.addEventListener('load',function(){
+	size();
+});
 
-containerDOM += `<div class="container">`; 
-
-for (i=0, i<100, i++) { 
-    containerDOM += `<div class="one"></div>`
+function randomFromArray(arr){
+  return arr[Math.floor(Math.random()*arr.length)]
 }
 
+function size(){
+	var container = document.getElementById('container');
+  container.innerHTML = '';
+	var x = Math.floor(window.innerWidth/20);
+  var y = Math.floor(window.innerHeight/20);
+  for(var i = 0; i < x*y; i++){
+  	var box = document.createElement('div');
+    box.className = 'box';
+    container.appendChild(box);
+    
+    box.style.padding = window.innerWidth/20 + "px";
+    box.style.backgroundColor = randomFromArray(colorArray);
+    // box.style.height = window.innerHeight/20 + "px";
+    // box.style.width = window.innerHeight/20 + "px";
+  }
+}
+
+window.addEventListener('resize',function(){
+	size();
+});
