6729b2e452048054ff53dffccb3fc21dea3512f0
anushkavhatkar <vhata986@newschool.edu>
2023-04-26 20:44:42 -0400

    [MAIN] testing js

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const APP_TOKEN = 'YEAf7HhwEpQfNrdeSPEwxXv58';
 
 const DATASET_IDENTIFIER = 'uvpi-gqnh';
 
+const mainContainer = document.querySelector('#container');
 
+let treesDOM = "<div>";
 
 // const url = `https://data.cityofnewyork.us/resource/${DATASET_IDENTIFIER}.json?$limit=${LIMIT}&$$app_token=${APP_TOKEN}`;
 
@@ -15,5 +17,24 @@ console.log(`Fetching url - ${url}`);
 fetch(url)
   .then((response) => response.json())
   .then((json) => {
-      console.log(json);
+    
+    console.log(json);
+
+    treesDOM += `<div>`
+
+    json.forEach(function (tree) { 
+        if (tree.spc_common == "pin oak") {
+            treesDOM += `<div class="pin-oak"> (${tree.tree_id}), (${tree.spc_common}), (${tree.spc_latin})
+            </div>`;
+        }
+
+        
+
+
+    });
+
+    treesDOM += `</div>`
+    treesDOM += `</div>`
+    mainContainer.innerHTML = treesDOM;
+
   });
