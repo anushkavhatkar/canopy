b4f3d15f71e560423911c2141f29c4b3f748df10
anushkavhatkar <vhata986@newschool.edu>
2023-04-29 16:47:49 -0400

    [main js] added array for all tree species

--- main.js
+++ main.js
@@ -8,6 +8,93 @@ const LIMIT = 580000;
 
 let treesDOM = "<div>";
 
+/* ALL TREE SPECIES ARRAY LIST */
+
+
+let pinOakArray = [];
+let redMapleArray = [];
+let honeylocustArray = [];
+let americanLindenArray = [];
+let londonPlanetreeArray = [];
+let ginkgosArray = [];
+let americanElmArray = [];
+let sophoraArray = [];
+let calleryPearArray = [];
+let littleleafLindenArray = [];
+let fastigiataOakArray = []; 
+let sweetgumArray = [];
+let dawnRedwoodArray = [];
+let baldcypressArray = [];
+let coffeetreesArray = [];
+let northernRedOakArray = [];
+let swampWhiteOakArray = [];
+let shingleOakArray = []; 
+let willowOakArray = [];
+let crimeanLindenArray = [];
+let silverLindenArray = [];
+let riverBirchArray = [];
+let hackberryArray = [];
+let katsuraTreeArray = [];
+let turkishHazelnutArray = [];
+let hardyRubberTreeArray = [];
+let europeanBeechArray = [];
+let kentuckyCoffeetreeArray = [];
+let tulipTreeArray = [];
+let blackgumArray = [];
+let sawtoothOakArray = [];
+let whiteOakArray = [];
+let scarletOakArray = [];
+let burOakArray = [];
+let englishOakArray = [];
+let redOakArray = [];
+let shumardOakArray = [];
+let blackOakArray = [];
+let baldCypressArray = [];
+let chineseElmArray = [];
+let japaneseZelkovaArray =[];
+let redHorsechestnut = [];
+let europeanHornbeamArray = [];
+let americanHornbeamArray = [];
+let japaneseHornbeamArray = [];
+let kentuckyYellowwoodArray = [];
+let goldenRaintreeArray = [];
+let amurMaackiaArray = [];
+let persianIronwoodArray = [];
+let hedgeMapleArray = [];
+let amurMapleArray = [];
+let paperbarkMapleArray = [];
+let tartarMapleArray = [];
+let shantungMapleArray = [];
+let serviceberryArray = [];
+let redbudArray = [];
+let chineseFringetreeArray = [];
+let kousaDogwoodArray = [];
+let cornelianCherryArray = [];
+let cockspurHawthornArray = [];
+let crabappleArray = [];
+let purpleLeafPlumArray = [];
+let japaneseTreeLilacArray = [];
+let chineseTreeLilacArray = [];
+
+
+
+// let yellowBuckeyeArray = [];
+// let kwanzanCherryArray = [];
+// let snowGooseCherryArray = [];
+// let canadaRedCherryArray = [];
+// let yoshinoCherryArray = [];
+// let okameCherryArray = [];
+// let sargentCherryArray = [];
+// let persianParrotiaArray = [];
+// let koreanStewartiaArray = [];
+// let elmHybridsArray = [];
+// let nuttallOakArray = [];
+// let chinkapinOakArray = [];
+// let chestnutOakArray = [];
+// let daimioOakArray = [];
+// let italianOakArray = [];
+
+/*                                      */
 // const url = `https://data.cityofnewyork.us/resource/${DATASET_IDENTIFIER}.json?$limit=${LIMIT}&$$app_token=${APP_TOKEN}`;
 
 
