b6173413289accb49d87286ef3398b28933dd07b
anushkavhatkar <vhata986@newschool.edu>
2023-04-29 23:12:18 -0400

    [main] data viz trial one

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const APP_TOKEN = 'YEAf7HhwEpQfNrdeSPEwxXv58';
 
 const DATASET_IDENTIFIER = 'uvpi-gqnh';
 
-const mainContainer = document.querySelector('#container');
+let mainContainer = document.querySelector('#container');
 
 const LIMIT = 580000; 
 
@@ -11,8 +11,10 @@ let treesDOM = " ";
 
 
 
+
 /* ------------------------------------ ALL TREE SPECIES ARRAYS ------------------------------------ */
 
+let allArray = [];
 
 let pinOakArray = [];
 let redMapleArray = [];
@@ -81,23 +83,6 @@ let chineseTreeLilacArray = [];
 
 
 
-// let yellowBuckeyeArray = [];
-// let kwanzanCherryArray = [];
-// let snowGooseCherryArray = [];
-// let canadaRedCherryArray = [];
-// let yoshinoCherryArray = [];
-// let okameCherryArray = [];
-// let sargentCherryArray = [];
-// let persianParrotiaArray = [];
-// let koreanStewartiaArray = [];
-// let elmHybridsArray = [];
-// let nuttallOakArray = [];
-// let chinkapinOakArray = [];
-// let chestnutOakArray = [];
-// let daimioOakArray = [];
-// let italianOakArray = [];
-
-
 
 /* ------------------------------------  TREE COLOR ARRAYS  ------------------------------------ */
 
@@ -109,16 +94,17 @@ let nativeColorsArray = ["#2C4A43", "#005E2B", "#143514", "#428400", "#52762F", 
 let treeColorsArray = [ "#2ADB31", "#B3ED39", "#D5F21F", "#ACEB44", "#02E34F", "#00CE78", "#0AFA5C", "#0CF090", "#DBFF00", "#80FF00", "#00FFA3", "#6AFBA4", "#5DD8B3", "#00CF08", "#C9E518", "#EBEE6D","#01AF5A", "#00FF19", "#20E0A6", "#20E084", "#6BEE90", "#B3ECB2", "#76DAA4", "#5CE05A", "#00FF75","#91FF8E", "#B0FF62", "#93FF26","#76EC00","#D8FAA0","#CDFF7A","#14B87D", "#00C766", "#2C4A43", "#005E2B", "#143514", "#428400", "#52762F", "#1D6249", "#415C1E", "#3D4A08", "#145033", "#293A18", "#70903C", "#4C7345", "#327B10", "#325F1E", "#004006", "#697A02", "#3F4420", "#707649" ]
 
 
-// const url = `https://data.cityofnewyork.us/resource/${DATASET_IDENTIFIER}.json?$limit=${LIMIT}&$$app_token=${APP_TOKEN}`;
 
 
-// const url = `https://data.cityofnewyork.us/resource/${DATASET_IDENTIFIER}.json?$$app_token=${APP_TOKEN}&$query=SELECT%20%60tree_id%60%2C%20%60spc_latin%60%2C%20%60spc_common%60%0AWHERE%0A%20%20(%60tree_id%60%20%3D%3D%20%60tree_id%60)%0A%20%20AND%20((%60spc_latin%60%20%3D%3D%20%60spc_latin%60)%20AND%20(%60spc_common%60%20%3D%3D%20%60spc_common%60))`;
+
+
+/* ------------------------------------  API FETCH  ------------------------------------ */
 
 const url = `https://data.cityofnewyork.us/resource/${DATASET_IDENTIFIER}.json?$$app_token=${APP_TOKEN}&$limit=${LIMIT}`;
 
-// const url = `https://data.cityofnewyork.us/resource/${DATASET_IDENTIFIER}.json?$$app_token=${APP_TOKEN}`;
 
 console.log(`Fetching url - ${url}`);
+
 
 fetch(url)
   .then((response) => response.json())
@@ -126,60 +112,64 @@ fetch(url)
     
     console.log(json);
 
-    treesDOM += `<div>`
+    
 
     json.forEach(function (tree) { 
         if (tree.spc_common == "pin oak") {
-            // treesDOM += `<div class="pin-oak"> (${tree.tree_id}), (${tree.spc_common}), (${tree.spc_latin})
-            // </div>`;
             pinOakArray.push(tree);
-
         }
 
         if (tree.spc_common == "red maple") {
-            // treesDOM += `<div class="red-maple"> (${tree.tree_id}), (${tree.spc_common}), (${tree.spc_latin})
-            // </div>`;
             redMapleArray.push(tree);
         }
 
-        // if (tree.spc_common == "honeylocust") {
-        //     treesDOM += `<div class="honeylocust"> (${tree.tree_id}), (${tree.spc_common}), (${tree.spc_latin})
-        //     </div>`;
-        // }
+        if (tree.spc_common == "honeylocust") {
+            
+        }
 
-        // if (tree.spc_common == "American linden") {
-        //     treesDOM += `<div class="american-linden"> (${tree.tree_id}), (${tree.spc_common}), (${tree.spc_latin})
-        //     </div>`;
-        // }
+        if (tree.spc_common == "American linden") {
+    
+            ;
+        }
 
-        // if (tree.spc_common == "London planetree") {
-        //     treesDOM += `<div class="london-planetree"> (${tree.tree_id}), (${tree.spc_common}), (${tree.spc_latin})
-        //     </div>`;
-        // }
+        if (tree.spc_common == "London planetree") {
+            
+        }
 
-        // if (tree.spc_common == "ginkgo") {
-        //     treesDOM += `<div class="ginkgo"> (${tree.tree_id}), (${tree.spc_common}), (${tree.spc_latin})
-        //     </div>`;
-        // }
+        if (tree.spc_common == "ginkgo") {
+            
+        }
 
 
     });
 
-    // treesDOM += `</div>`
+
+    treesDOM += `<div class="tree-box">`;
+
+    
+    
+    for (i = 0; i < Math.floor(pinOakArray.length/100); i++) {
+    treesDOM += `<div class="box"></div>`
+    };
+
+
+
     treesDOM += `</div>`
+
+
     mainContainer.innerHTML = treesDOM;
 
-    // const findOak = json.some((tree) => {
-    //     return tree.spc_common == "pin oak";
-    // })
-
-    // console.log(findOak);
-    // console.log(findOak.length);
+    allArray.push(pinOakArray, redMapleArray);
     
     console.log(pinOakArray.length);
     console.log(pinOakArray);
     console.log(redMapleArray.length);
     console.log(redMapleArray);
+
+    console.log(allArray.length);
+    console.log(allArray);
+
+    console.log(treesDOM);
     
   });
 
